42e9cc0375ec67e719d2caf9dd2b1687c9f281fa
timosamedet <timosamedet@yahoo.com>
2025-05-28 10:58:09 +0100

    Added Vas Utility Resource and Vending permissions

--- src/main/resources/db/changelog/version1/02_db_data.sql
+++ src/main/resources/db/changelog/version1/02_db_data.sql
@@ -11,3 +11,17 @@ VALUES
 INSERT IGNORE INTO application_configurations(id, name, value, created_at)
 VALUES
     (2, 'bulk_name_inquiry_processing_block', 'false', NOW());
+
+
+
+--changeset ABS:AddedVasUtilityResource
+INSERT IGNORE INTO resources(id, uuid, name, deleted, description, date_created)
+VALUES
+    (20, 'b6f5eb90-7214-4542-b977-ff990d7fb53d','VAS Utility', 0, 'TP IBanking VAS Utility Resource', NOW());
+
+
+--changeset ABS:AddedVasUtilityVendingPermissions
+INSERT IGNORE INTO permissions(id, uuid, name, authority, deleted, description, date_created, resource_id, permission_type)
+VALUES
+    (175, '8c6623c2-7ee7-4884-95f5-92170f0e2eb5', 'Initiate VAS Utility Vending', 'TP_IB::BUS_CLIENT::_vas::initiate_vas_vending', 0, 'User has authority to initiate vas utility vending', NOW(), 20, 'BUSINESS_CLIENT'),
+    (176, '6d5973c8-6934-40a0-a6af-7b29f691ad48', 'Confirm VAS Utility Vending', 'TP_IB::BUS_CLIENT::_vas::confirm_vas_vending', 0, 'User has authority to confirm vas utility vending', NOW(), 20, 'BUSINESS_CLIENT');
